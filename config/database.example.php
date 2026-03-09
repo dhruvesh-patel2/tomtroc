0647b68076048debbe13d1266996e18e6197efa2
dhruvesh <Dpatel@outlook.Fr>
2026-03-09 18:42:33 +0100

    corectif manquant fi,

---
+++ config/database.example.php
@@ -0,0 +1,12 @@
+<?php
+    // Ici on met les constantes utiles, 
+    // les données de connexions à la bdd
+    // et tout ce qui sert à configurer. 
+
+    define('TEMPLATE_VIEW_PATH', './views/templates/'); // Le chemin vers les templates de vues.
+    define('MAIN_VIEW_PATH', TEMPLATE_VIEW_PATH . 'main.php'); // Le chemin vers le template principal.
+
+    define('DB_HOST', 'CHANGE_ME'); // Le chemin vers la base de données.
+    define('DB_NAME', 'CHANGE_ME');
+    define('DB_USER', 'CHANGE_ME');
+    define('DB_PASS', 'CHANGE_ME');
